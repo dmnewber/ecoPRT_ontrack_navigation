541d5e9a45b04ea3989f86175c9e9c4ffa15c6cc
root <root@beaglebone.localdomain>
2015-10-04 01:13:48 +0000

    Added wiringBone library.

---
+++ wiringBone/gpiotest.cpp
@@ -0,0 +1,16 @@
+
+
+void setup(){
+//	pinMode(P8_7,OUTPUT);
+//	digitalWrite(P8_7,HIGH);
+//	setFrequency(P8_13,100);
+//	setDutyPercentage(P8_13,30);
+	setPulseWidth(P8_13,1000);
+}
+int ADC_read;
+void loop(){
+//	digitalWrite(P8_7,LOW);
+//	digitalWrite(P8_7,HIGH);
+//	ADC_read = analogRead(AIN0);
+//	printf("%d\n", ADC_read);
+}
